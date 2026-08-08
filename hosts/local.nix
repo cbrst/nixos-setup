fb57fcec815776e3195fbc2f3646e8cd2c2d4be1
Christian Brassat <christian@brassat.eu>
2026-08-08 10:14:47 +0200

    Track hosts/local.nix

---
+++ hosts/local.nix
@@ -0,0 +1,8 @@
+# Defaults for evaluating the flake from a Git checkout. The installer overwrites
+# this file in the target system with the values selected during installation.
+{
+  user = "cbrst";
+  hostName = "Asgard";
+  timeZone = "Europe/Berlin";
+  keyMap = "us";
+}
