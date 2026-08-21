c7ce3bf64b3c047cb1de3866f7d3aee5a802d37e
Christian Brassat <christian@brassat.eu>
2026-08-21 23:25:08 +0200

    feat: support Steam sessions on Niri
    Enable Xbox One controller support and install xwayland-satellite so Steam can run under Niri. Provide the SteamOS session-selection helper so Steam's Switch to Desktop action terminates the current Gamescope session.\n\nValidated with nix flake check --no-build and git diff --check.

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -3,6 +3,9 @@
 # `imports` to disable.
 { pkgs, ... }:
 {
+  # enable support for Xbox One controllers
+  hardware.xone.enable = true;
+
   programs.steam = {
     enable = true;
     gamescopeSession.enable = true;
@@ -14,9 +17,25 @@
   programs.xwayland.enable = true;
 
   environment.systemPackages = with pkgs; [
+    # small steamos-session-select to exit out of a gamescope session
+    (writeShellScriptBin "steamos-session-select" ''
+      case "''${1:-}" in
+        desktop)
+          # Steam's "Switch to Desktop" action uses this SteamOS helper.
+          exec ${systemd}/bin/loginctl terminate-session "$XDG_SESSION_ID"
+          ;;
+        *)
+          exit 1
+          ;;
+      esac
+    '')
+
     mangohud
     protonup-qt
     vulkan-tools
     mesa-demos
+
+    # needed for Steam in Niri
+    xwayland-satellite
   ];
 }
